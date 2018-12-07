439ae34dfd15915a08849b61dd912e39bd81ff55
ashinzekene <ashinzekene@gmail.com>
2018-12-07 14:01:55 +0100

    bubble sort go

--- sorting/bubble-sort/index.go
+++ sorting/bubble-sort/index.go
@@ -18,3 +18,19 @@ func bubbleSort(arr []int) []int {
   }
   return arr
 }
+
+// Faster bubble sort
+func bubbleSort(arr []int) []int {
+  end = 1
+	for x := 0; x < len(arr); x++ {
+    for i := 0; i < len(arr) - end;i++ {
+      if arr[i] > arr[i + 1] {
+        temp := arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+      }
+      end++
+    }
+  }
+  return arr
+}
