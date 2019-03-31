83cb85f614fcfb02500bea21fb6b86c3c91ac38b
ashinzekene <ashinzekene@gmail.com>
2019-03-31 04:37:26 +0100

    Add test for countingValleys

--- counting-valleys/index.go
+++ counting-valleys/index.go
@@ -1,16 +1,10 @@
 package algorithms
 
-import "fmt"
-
-func main() {
-	fmt.Println(countingValleys(1, "UDDDUDUU"))
-}
-
-func countingValleys(n int32, s string) int32 {
+func CountingValleys(n int32, s string) int32 {
 	counter := 0
 	var result int32 = 0
 	for i := 0; i < len(s); i++ {
-		if s[i] == D {
+		if string(s[i]) == "D" {
 			counter--
 		} else {
 			counter++
---
+++ counting-valleys/index_test.go
@@ -0,0 +1,30 @@
+package algorithms
+
+import (
+	"testing"
+	
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	testCases := []struct {
+		Path     string
+		Steps    int32
+		Expected int32
+	}{
+		{
+			"UDDDUDUU",
+			8,
+			1,
+		},
+		{
+			"DDUUDDUDUUUD",
+			12,
+			2,
+		},
+	}
+	for _, tc := range testCases {
+		result := CountingValleys(tc.Steps, tc.Path)
+		assert.Equal(t, tc.Expected, result, "Failed for "+tc.Path)
+	}
+}
