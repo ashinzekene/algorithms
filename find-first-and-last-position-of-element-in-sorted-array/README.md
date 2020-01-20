d103e8aa6c6b4200e8b62df0b73e4cbf7df54fc4
Ekene Ashinze <ashinzekene@gmail.com>
2020-01-20 16:26:46 +0100

    better algo for find first and last...

--- find-first-and-last-position-of-element-in-sorted-array/README.md
+++ find-first-and-last-position-of-element-in-sorted-array/README.md
@@ -1,6 +1,8 @@
 # Find First and Last Position of Element in Sorted Array
 **Medium**
 
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
 Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 
 Your algorithm's runtime complexity must be in the order of `O(log n)`.
--- find-first-and-last-position-of-element-in-sorted-array/index.go
+++ find-first-and-last-position-of-element-in-sorted-array/index.go
@@ -1,29 +1,23 @@
 package algorithms
 
 func searchRange(nums []int, target int) []int {
+	i := 0
+	j := len(nums) - 1
 	result := []int{-1, -1}
-	if len(nums) == 0 {
-		return result
-	}
-	left := getExtreme(nums, target, true)
-	if left == len(nums) || nums[left] != target {
-		return result
-	}
-	result[0] = left
-	result[1] = getExtreme(nums, target, false) - 1
-	return result
-}
-
-func getExtreme(nums []int, target int, left bool) int {
-	l := 0
-	r := len(nums)
-	for l < r {
-		m := (l + r) / 2
-		if nums[m] > target || (nums[m] == target && left) {
-			r = m
+	for i <= j {
+		if result[0] > -1 && result[1] > -1 {
+			return result
+		}
+		if target == nums[i] {
+			result[0] = i
 		} else {
-			l = m + 1
+			i++
+		}
+		if target == nums[j] {
+			result[1] = j
+		} else {
+			j--
 		}
 	}
-	return l
+	return result
 }
