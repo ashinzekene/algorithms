cb4f3a1b17cba6243b2f708a2b646fe3e339be82
Ekene Ashinze <ashinzekene@gmail.com>
2020-02-02 20:21:35 +0100

    add set matrix zeros

---
+++ set-matrix-zeroes/index.go
@@ -0,0 +1,30 @@
+package algorithms
+
+func setZeroes(matrix [][]int) [][]int {
+	rows := make(map[int]bool, 0)
+	cols := make(map[int]bool, 0)
+	if len(matrix) == 0 {
+		return matrix
+	}
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			if matrix[i][j] == 0 {
+				rows[i] = true
+				cols[j] = true
+			}
+		}
+	}
+
+	for row := range rows {
+		for i := range matrix[row] {
+			matrix[row][i] = 0
+		}
+	}
+
+	for col := range cols {
+		for row := range matrix {
+			matrix[row][col] = 0
+		}
+	}
+	return matrix
+}
---
+++ set-matrix-zeroes/index_test.go
@@ -0,0 +1,70 @@
+package algorithms
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_LeftRotation(t *testing.T) {
+	testCases := []struct {
+		Nums     [][]int
+		Expected [][]int
+	}{
+		{
+			[][]int{
+				[]int{0, 1, 2, 0},
+				[]int{3, 4, 5, 2},
+				[]int{1, 3, 1, 5},
+			},
+			[][]int{
+				[]int{0, 0, 0, 0},
+				[]int{0, 4, 5, 0},
+				[]int{0, 3, 1, 0},
+			},
+		},
+		{
+			[][]int{
+				[]int{1, 3},
+				[]int{0, 6},
+			},
+			[][]int{
+				[]int{0, 3},
+				[]int{0, 0},
+			},
+		},
+		{
+			[][]int{
+				[]int{1, 4},
+				[]int{2, 3},
+			},
+			[][]int{
+				[]int{1, 4},
+				[]int{2, 3},
+			},
+		},
+		{
+			[][]int{
+				[]int{1},
+			},
+			[][]int{
+				[]int{1},
+			},
+		},
+		{
+			[][]int{
+				[]int{1, 4},
+				[]int{0, 0},
+			},
+			[][]int{
+				[]int{0, 0},
+				[]int{0, 0},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		actual := setZeroes(tc.Nums)
+		assert.Equal(t, tc.Expected, actual, fmt.Sprintf("Did not work for %v", tc.Nums))
+	}
+}
