8b1d6e5afbf2cb62f53cdfa3f6cf743134cbf91c
Ekene Ashinze <ashinzekene@gmail.com>
2021-08-08 15:20:27 +0100

    Edit valid sudoku

--- valid-sudoku/README.md
+++ valid-sudoku/README.md
@@ -1,17 +1,22 @@
 # Valid Sudoku
-**Medium**
+**Medium**  
+https://leetcode.com/problems/valid-sudoku/  
+
 
 Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:  
 
-Each row must contain the digits 1-9 without repetition.
-Each column must contain the digits 1-9 without repetition.
-Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.  
+- Each row must contain the digits 1-9 without repetition.
+- Each column must contain the digits 1-9 without repetition.
+- Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.  
 
-A partially filled sudoku which is valid.  
+**Note:**
 
-The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+- A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+- Only the filled cells need to be validated according to the mentioned rules.
 
 Example 1:
+
+![Sudoku Image](./sudoku.png)
 ```
 Input:
 [
@@ -45,9 +50,8 @@ Output: false
 Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 ```
 
-**Note:**
+**Constraints:**
 
-* A Sudoku board (partially filled) could be valid but is not necessarily solvable.
-* Only the filled cells need to be validated according to the mentioned rules.
-* The given board contain only digits 1-9 and the character '.'.
-* The given board size is always 9x9.
+- `board.length == 9`
+- `board[i].length == 9`
+- `board[i][j] is a digit or '.'.`
--- valid-sudoku/index.go
+++ valid-sudoku/index.go
@@ -1,65 +1,51 @@
 package algorithms
 
 func isValidSudoku(board [][]byte) bool {
-	for i, row := range board {
-		// Check if colum ns are valid
-		validCol := isValidCol(row)
-		if !validCol {
-			return false
+	// Validate horizontally
+	for _, row := range board {
+		byteIndex := make(map[byte]int)
+		for _, b := range row {
+			byteIndex[b]++
 		}
-		validRow := checkValidRow(board, i)
-		if !validRow {
+		if !validateByteIndex(byteIndex) {
 			return false
 		}
 	}
-	var cell [3][3]byte
-	for l := 0; l < len(board); l += 3 {
-		for k := 0; k < len(board); k += 3 {
-			for i := l; i < l+3; i++ {
-				for j := k; j < k+3; j++ {
-					cell[i%3][j%3] = board[i][j]
-				}
-			}
-			valid := checkValid3Cell(cell)
-			if !valid {
-				return false
-			}
+
+	// Validate vertically
+	for i := range board[0] {
+		byteIndex := make(map[byte]int)
+		for _, col := range board {
+			byteIndex[col[i]]++
+		}
+		if !validateByteIndex(byteIndex) {
+			return false
 		}
 	}
+
+	// Validate 3by3 matrix
+	for count := 0; count < 9*3; count += 3 {
+		jStart := (count / 9) * 3
+		iStart := count % 9
+
+		byteIndex := make(map[byte]int)
+		for i := iStart; i < iStart+3; i++ {
+			for j := jStart; j < jStart+3; j++ {
+				byteIndex[board[i][j]]++
+			}
+		}
+		if !validateByteIndex(byteIndex) {
+			return false
+		}
+	}
+
 	return true
 }
 
-func isValidCol(col []byte) bool {
-	byteMap := make(map[byte]bool)
-	for _, num := range col {
-		if byteMap[num] && num != '.' {
+func validateByteIndex(byteIndex map[byte]int) bool {
+	for val, count := range byteIndex {
+		if count > 1 && val != '.' {
 			return false
-		}
-		byteMap[num] = true
-	}
-	return true
-}
-
-func checkValidRow(board [][]byte, i int) bool {
-	byteMap := make(map[byte]bool)
-	for _, col := range board {
-		if byteMap[col[i]] && col[i] != '.' {
-			return false
-		}
-		byteMap[col[i]] = true
-	}
-	return true
-}
-
-func checkValid3Cell(cell [3][3]byte) bool {
-	byteMap := make(map[byte]bool)
-	for i := 0; i < len(cell); i++ {
-		for j := 0; j < len(cell[0]); j++ {
-			val := cell[i][j]
-			if byteMap[val] && val != '.' {
-				return false
-			}
-			byteMap[val] = true
 		}
 	}
 	return true
