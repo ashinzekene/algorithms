e2201f4deb6ca9a88287b2483279a101b4edd30f
Ekene Ashinze <ashinzekene@gmail.com>
2021-10-17 14:40:24 +0100

    Fix max product of three

--- max-product-of-three/index.go
+++ max-product-of-three/index.go
@@ -1,15 +1,17 @@
 package algorithms
 
+import "sort"
+
 func maxProductThree(A []int) int {
-    sort.Ints(A)
-    l := len(A)
-    positiveMax := A[l-1] * A[l-2]* A[l-3]
-    if A[0] >= 0 {
-        return positiveMax
-    }
-    negativeMax := A[0] * A[1] * A[l-1]
-    if positiveMax > negativeMax {
-        return positiveMax
-    }
-    return negativeMax
+	sort.Ints(A)
+	l := len(A)
+	positiveMax := A[l-1] * A[l-2] * A[l-3]
+	if A[0] >= 0 {
+		return positiveMax
+	}
+	negativeMax := A[0] * A[1] * A[l-1]
+	if positiveMax > negativeMax {
+		return positiveMax
+	}
+	return negativeMax
 }
