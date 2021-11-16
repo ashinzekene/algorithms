7af85bfd29c8963f5f6a2611a479714ee4131aa1
Ekene Ashinze <ashinzekene@gmail.com>
2021-11-16 07:20:20 +0100

    Fix

--- integer-to-roman/index_test.go
+++ integer-to-roman/index_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_whatFlavors(t *testing.T) {
+func Test_intToRoman(t *testing.T) {
 	testCases := []struct {
 		Number   int
 		Expected string
