182084103924d4e60221732f258edd01aedaeefb
Ekene Ashinze <ashinzekene@gmail.com>
2021-11-27 07:31:13 +0100

    Remove unused code from subet

--- subset/index.go
+++ subset/index.go
@@ -2,14 +2,13 @@ package algorithms
 
 func Subsets(nums []int) [][]int {
 	result := [][]int{}
-	used := make(map[int]bool)
 	for length := 0; length <= len(nums); length++ {
-		addSubSet(&result, []int{}, used, nums, length, 0)
+		addSubSet(&result, []int{}, nums, length, 0)
 	}
 	return result
 }
 
-func addSubSet(result *[][]int, currentSubSet []int, used map[int]bool, numsLeft []int, length, start int) {
+func addSubSet(result *[][]int, currentSubSet []int, numsLeft []int, length, start int) {
 	if len(currentSubSet) == length {
 		subset := make([]int, len(currentSubSet))
 		copy(subset, currentSubSet)
@@ -17,10 +16,6 @@ func addSubSet(result *[][]int, currentSubSet []int, used map[int]bool, numsLeft
 		return
 	}
 	for i := start; i < len(numsLeft); i++ {
-		if !used[i] {
-			used[i] = true
-			addSubSet(result, append(currentSubSet, numsLeft[i]), used, numsLeft, length, i+1)
-			used[i] = false
-		}
+		addSubSet(result, append(currentSubSet, numsLeft[i]), numsLeft, length, i+1)
 	}
 }
