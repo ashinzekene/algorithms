050833046d412e38db5885c24d8574eb02d3f4ec
Ekene Ashinze <ashinzekene@gmail.com>
2022-01-11 06:42:28 +0100

    Fix unnecssary assignment

--- k-radius-subarray-averages/index.go
+++ k-radius-subarray-averages/index.go
@@ -7,7 +7,7 @@ func getAverages(nums []int, k int) []int {
 	for i, v := range nums {
 		cummulativeNums[i+1] = cummulativeNums[i] + v
 	}
-	for i, _ := range nums {
+	for i := range nums {
 		if i-k < 0 || i+k >= l {
 			result[i] = -1
 			continue
